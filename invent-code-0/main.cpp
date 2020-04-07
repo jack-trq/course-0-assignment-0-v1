47c66d948dbc8170c57bb489f9cdc82af4dbf0fe
Jack Arendt <jack@therightquestion.dev>
2020-04-07 22:36:01 +0000

    Updating all questions

--- invent-code-0/main.cpp
+++ invent-code-0/main.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main() {
-  // Write code here.
+  cout << "Hi, my name is Jack.";
+  cout << endl;
+  cout << "When not at the computer, I like to run and bike.";
+  cout << endl;
+  cout << "I have a 13lb mini dachshund named Bailey.";
+  cout << endl;
 }
--- invent-code-1/main.cpp
+++ invent-code-1/main.cpp
@@ -3,5 +3,8 @@
 using namespace std;
 
 int main() {
-  // Write code here.
+  cout << "Hi, my name is Jack.";
+  cout << " When not at the computer, I like to run and bike.";
+  cout << " I have a 13lb mini dachshund named Bailey.";
+  cout << endl;
 }
